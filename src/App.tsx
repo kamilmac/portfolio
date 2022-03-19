0fa260e6689af978472ed34400a60eb6960d8569
kamilmac <kmacinski@gmail.com>
2022-03-19 11:55:35 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+
+const Box: React.FC<JSX.IntrinsicElements['mesh']> = (props) => {
+  const ref = React.useRef()
+  const [hovered, hover] = React.useState(false)
+  const [clicked, click] = React.useState(false)
+  useFrame((state, delta) => {
+    ref.current.rotation.x += 0.01
+  })
+
+  return (
+    <>
+      <mesh
+        {...props}
+        ref={ref}
+        scale={clicked ? 1.5 : 1}
+        onClick={(event) => click(!clicked)}
+        onPointerOver={(event) => hover(true)}
+        onPointerOut={(event) => hover(false)}
+      >
+        <sphereGeometry args={[1, 16, 16]} />
+        <meshNormalMaterial flatShading />
+      </mesh>
+    </>
+  )
+}
+
+export const App: React.FC = () => {
+  return (
+    <Canvas
+      style={{
+        height: '100vh',
+      }}
+    >
+      <Light />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+      <camera position={[3,3,3]}/>
+    </Canvas>
+  );
+}
+
+const Light = () => {
+  const ref = React.useRef()
+  useFrame((state, delta) => {
+    if (ref?.current) {
+      ref.current.position.y += 0.01
+    }
+  })
+  return (
+    <pointLight ref={ref} position={[-1, 10, 10]} color="red" intensity={4}/>
+  )
+}
