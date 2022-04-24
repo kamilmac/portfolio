d89cdb10f5e34e4bcd69bd7bd7bf839040e60542
kamilmac <kmacinski@gmail.com>
2022-04-24 22:14:32 +0200

    bugfix

--- src/sketches/1.tsx
+++ src/sketches/1.tsx
@@ -1,34 +1,81 @@
 import React from 'react'
-import { Canvas } from '@react-three/fiber'
-import { sculptToMinimalRenderer } from 'shader-park-core/dist/shader-park-core.esm.js';
+import { Canvas, useFrame } from '@react-three/fiber'
+
+const COLORS = [
+  '#9B2915',
+  '#E9B44C',
+  '#E4D6A7',
+  '#1C110A',
+  '#50A2A7'
+];
+
+const NUM_OF_BOXES = 800;
+const MAX_POSITION_DEVIATION = 10;
+
+const boxes = [...Array(NUM_OF_BOXES).fill({})]
+  .map(b => ({
+    color: COLORS[Math.floor(Math.random() * COLORS.length)],
+    position: [
+      Math.floor(Math.random() * (MAX_POSITION_DEVIATION*2) - MAX_POSITION_DEVIATION),
+      Math.floor(Math.random() * (MAX_POSITION_DEVIATION*2) - MAX_POSITION_DEVIATION),
+      Math.floor(Math.random() * (MAX_POSITION_DEVIATION*2) - MAX_POSITION_DEVIATION),
+    ],
+  }));
+
+const Box: React.FC<JSX.IntrinsicElements['mesh']> = (props) => {
+  const ref = React.useRef()
+  const [hovered, hover] = React.useState(false)
+  const [clicked, click] = React.useState(false)
+
+  useFrame((state, delta) => {
+    // ref.current.rotation.x += 0.01
+  })
+
+  return (
+    <>
+      <mesh
+        {...props}
+        ref={ref}
+        scale={clicked ? 1.5 : 1}
+        onClick={(event) => click(!clicked)}
+        onPointerOver={(event) => hover(true)}
+        onPointerOut={(event) => hover(false)}
+      >
+        <boxGeometry args={props.position} />
+        <meshBasicMaterial color={props.color} />
+      </mesh>
+    </>
+  )
+}
 
 const App: React.FC = () => {
-  React.useEffect(() => {
-    const canvas = document.getElementsByTagName('Canvas')[0];
-    
-    sculptToMinimalRenderer(canvas, spCode);
-  }, []);
   return (
-    <canvas
+    <Canvas
       style={{
         height: '100vh',
       }}
-    />
+    >
+      <Light />
+      {
+        boxes.map(b => (
+          <Box position={b.position} color={b.color}/>
+        ))
+      }
+      <orthographicCamera position={[6,6,6]}/>
+    </Canvas>
   );
 }
 
-function spCode() {
-  //Put your Shader Park Code here
-  setStepSize(0.2);
-  let buttonHover = input();
-  let click = input();
-  let n = noise(getSpace() * 89 + buttonHover);
-  shine(10);
-  color(0.2 * normal * click + n * 0.2 + vec3(0, 0, 2));
-  sphere(0.3);
-  expand(n * 0.2);
-  mixGeo(buttonHover);
-  box(0.3, 0.3, 0.3);
+const Light = () => {
+  const ref = React.useRef()
+  useFrame((state, delta) => {
+    if (ref?.current) {
+      ref.current.position.y += 0.01
+    }
+  })
+  return (
+    <pointLight ref={ref} position={[-1, 10, 10]} color="red" intensity={4}/>
+  )
 }
 
 export default App;
