4280335442c2911413b27e386b6e65837544a794
kamilmac <kmacinski@gmail.com>
2022-06-02 20:08:16 +0200

    entry

--- src/sketches/9.tsx
+++ src/sketches/9.tsx
@@ -71,20 +71,40 @@ export default function App({ ...props }) {
         position={[633.46, 351.86, -930.44]}
       />
      
-      <PerspectiveCamera
-        name="Personal Camera"
-        makeDefault={true}
-        far={100000}
-        near={1}
-        fov={45}
-        up={[0, 1, 0]}
-        position={[15,5,0]}
-        rotation={[-2.38, 0.86, 2.51]}
-      />
+      <MyCam />
       <hemisphereLight name="Default Ambient Light" intensity={2.75} color="#eaeaea" position={[0, 1, 0]} />
       <EffectComposer>
         <DepthOfField focusDistance={0} focalLength={focalLength} bokehScale={bokehScale} height={512} />
       </EffectComposer>
     </Canvas>
+  )
+}
+
+
+const CAMvec = new THREE.Vector3(12,5,0);
+let lerp = true;
+
+const MyCam = () => {
+  const ref = React.useRef();
+  useFrame(() => {
+    lerp && ref.current.position.lerp(CAMvec, 0.06)
+  });
+  React.useEffect(() => {
+    setTimeout(() => {
+      lerp = false;
+    }, 2500)
+  }, [])
+  return (
+    <PerspectiveCamera
+      ref={ref}
+      name="Personal Camera"
+      makeDefault={true}
+      far={100000}
+      near={1}
+      fov={45}
+      up={[0, 1, 0]}
+      position={[200,100,0]}
+      rotation={[-2.38, 0.86, 2.51]}
+    />
   )
 }
