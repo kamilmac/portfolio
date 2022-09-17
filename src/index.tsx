3c989d5ab954147a9fc8d5df429c6080cd5b59a6
kamilmac <kmacinski@gmail.com>
2022-09-17 19:11:41 +0200

    17

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ import {
 } from "react-router-dom";
 import { createRoot } from 'react-dom/client';
 
-const RECENT_SKETCH = 16;
+const RECENT_SKETCH = 17;
 
 const Sketch = () => {
   const { id } = useParams();
