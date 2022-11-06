8932fccfdf30973dd1222ef819fde27ac956aefb
kamilmac <kmacinski@gmail.com>
2022-11-06 16:48:17 +0100

    adam

--- src/sketches/22.tsx
+++ src/sketches/22.tsx
@@ -21,7 +21,21 @@ export default function App() {
         dpr={window.devicePixelRatio}
       >
         {/* <Atom /> */}
-        <Letter />
+        <Letter
+          letter={'A'}
+        />
+        <Letter
+          letter={'D'}
+          offset={4}
+        />
+        <Letter
+          letter={'A'}
+          offset={8}
+        />
+        <Letter
+          letter={'M'}
+          offset={12}
+        />
         <Environment
           // background={true}
           preset='forest'
@@ -38,13 +52,13 @@ export default function App() {
           angle={0.6}
           position={[-4, 5, -15]}
         /> */}
-        <ambientLight intensity={1} />
+        <ambientLight intensity={0} />
 
         <OrbitControls
           minPolarAngle={0.5}
           maxPolarAngle={1.5}
           rotateSpeed={0.6}
-          autoRotate={true}
+          autoRotate
           autoRotateSpeed={0.3}
           enableDamping
         />
@@ -63,13 +77,51 @@ export default function App() {
   );
 };
 
-const Letter = () => {
-  const letterA = [
-    0, 1, 0, 0,
-    1, 0, 1, 0,
-    1, 1, 1, 0,
-    1, 0, 1, 0,
-  ];
+const Letter = ({letter, offset}) => {
+  const LETTERS = {
+    'A': [
+      3, 1, 2, 0,
+      1, 0, 1, 0,
+      1, 1, 1, 0,
+      1, 0, 1, 0,
+    ],
+    'B': [
+      1, 1, 2, 0,
+      1, 3, 4, 0,
+      1, 5, 2, 0,
+      1, 1, 4, 0,
+    ],
+    'C': [
+      3, 1, 2, 0,
+      4, 0, 0, 0,
+      2, 0, 0, 0,
+      5, 1, 4, 0,
+    ],
+    'D': [
+      1, 1, 2, 0,
+      1, 0, 1, 0,
+      1, 0, 1, 0,
+      1, 1, 4, 0,
+    ],
+    'E': [
+      1, 1, 2, 0,
+      1, 2, 0, 0,
+      1, 0, 0, 0,
+      1, 1, 1, 0,
+    ],
+    'F': [
+      1, 1, 1, 0,
+      1, 0, 0, 0,
+      1, 1, 0, 0,
+      1, 0, 0, 0,
+    ],
+    'M': [
+      1, 2, 3, 1,
+      1, 5, 4, 1,
+      1, 0, 0, 1,
+      1, 0, 0, 1,
+    ],
+  };
 
   const positionsX = [
     0, 1, 2, 3,
@@ -85,21 +137,90 @@ const Letter = () => {
     0, 0, 0, 0,
   ];
 
-  return (
-    <group>
-      {letterA.map((p, i) =>
-        !!p ?
-          <mesh position={[positionsX[i], positionsY[i], 0]} rotation={[0,Math.PI/ 2,0]} >
+  const select = (num, i) => {
+    switch (num) {
+      case 1:
+        return (
+          <group>
+            <mesh position={[positionsX[i], positionsY[i], 0]}>
+              <Atom />
+            </mesh>
+            <mesh position={[positionsX[i]+1, positionsY[i]+1, 0]} rotation={[0,0,Math.PI]}>
+              <Atom />
+            </mesh>
+          </group>
+        );
+      case 2:
+        return (
+          <group>
+            <mesh position={[positionsX[i], positionsY[i], 0]}>
+              <Atom />
+            </mesh>
+            <mesh position={[positionsX[i]+1, positionsY[i]+1, 0]} rotation={[0,0,Math.PI]}>
+              <Atom empty/>
+            </mesh>
+          </group>
+        );
+      case 3:
+        return (
+          <group>
+            <mesh position={[positionsX[i]+1, positionsY[i], 0]} rotation={[0,0,Math.PI/2]}>
+              <Atom />
+            </mesh>
+            <mesh position={[positionsX[i], positionsY[i]+1, 0]} rotation={[0,0,-Math.PI/2]}>
+              <Atom empty/>
+            </mesh>
+         </group>
+        );
+      case 4:
+        return (
+          <group>
+            <mesh position={[positionsX[i], positionsY[i]+1, 0]} rotation={[0,0,-Math.PI/2]}>
+              <Atom />
+            </mesh>
+            <mesh position={[positionsX[i]+1, positionsY[i], 0]} rotation={[0,0,Math.PI/2]}>
+              <Atom empty/>
+            </mesh>
+          </group>
+        );
+      case 5:
+        return (
+        <group>
+          <mesh position={[positionsX[i]+1, positionsY[i]+1, 0]} rotation={[0,0,-Math.PI]}>
             <Atom />
           </mesh>
-          : null
-          
+          <mesh position={[positionsX[i], positionsY[i], 0]}>
+            <Atom empty/>
+          </mesh>
+        </group>
+        );
+      default: return (
+        <group>
+          <mesh position={[positionsX[i], positionsY[i], 0]}>
+            <Atom empty/>
+          </mesh>
+          <mesh position={[positionsX[i]+1, positionsY[i]+1, 0]} rotation={[0,0,Math.PI]}>
+            <Atom empty/>
+          </mesh>
+        </group>
+      );
+    }
+  };
+
+  return (
+    <group
+      position={[offset ? offset : 0,0,0]}
+    >
+      {LETTERS[letter].map((p, i) =>
+        select(p, i)
       )}
     </group>
   );
 }
 
-const Atom = () => {
+const Atom = ({ empty }) => {
+  const [hovered, setHover] = React.useState(false)
+
   const vertices = new Float32Array( [
     0, 0, 0,
     0, 0, 1,
@@ -135,7 +256,12 @@ const Atom = () => {
   ] );
 
   return (
-    <mesh>
+    <mesh
+      rotation={[0,hovered ? Math.PI/4 : Math.PI/2,0]}
+      onPointerOver={() => setHover(true)}
+      onPointerOut={() => setTimeout(() => setHover(false), 800)}
+      scale={hovered ? 0.66 : 0.95}
+    >
       <bufferGeometry 
         attach="geometry"
       >
@@ -147,7 +273,7 @@ const Atom = () => {
         />
       </bufferGeometry>
       {/* <boxGeometry args={[1, 1, 1]} /> */}
-      <meshBasicMaterial attach="material" color="#ededed" side={THREE.DoubleSide} />
+      <meshBasicMaterial attach="material" color={empty ? 'hotpink' : '#ededed'} side={THREE.DoubleSide} />
     </mesh>
   );
 }
